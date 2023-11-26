1bc0a039a2932188f02531508c916991ef2c2296
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2023-11-26 23:39:04 +0100

    -

--- question3.js
+++ question3.js
@@ -1,2 +1,7 @@
 // 3. Skapa en div med ett B. Så här: <div>B</div>
 // Lägg det mellan A och C på html-sidan.
+
+
+const newDiv = document.createElement("div");
+newDiv.textContent = "B";
+document.getElementById("my-element").insertBefore(newDiv, document.getElementById("C"));
--- question4.js
+++ question4.js
@@ -4,5 +4,22 @@
 // Så här: <button class="primary">Button1</button> <button class="primary">Button2</button> ...
 // Koden kan använda en for-loop
 
+
+
+const buttonsContainer = document.getElementById("buttons-container");
+
+for (let i = 1; i <= 10; i++) {
+  const button = document.createElement("button");
+  button.textContent = `Button${i}`;
+  button.className = "primary";
+
 // 4.2 (2p) Lägg till addEventlistener på varje button.
 // Vid eventet "click" på button ska klassen ändras till "secondary"
+
+  button.addEventListener("click", function() {
+    button.classList.remove("primary");
+    button.classList.add("secondary");
+  });
+
+  buttonsContainer.appendChild(button);
+}
